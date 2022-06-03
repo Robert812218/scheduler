1250fc3a67b92f3019000d695d30d46a704c2100
Robert812218 <robert812@shaw.ca>
2022-06-03 12:36:02 -0600

    add axios.get etc

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,62 +4,84 @@ import DayList from "components/DayList";
 import "components/Application.scss";
 import "components/DayList";
 import Appointment from "./Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+// const appointments = {
+//   "1": {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   "2": {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer:{
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   "3": {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   "4": {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer:{
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   "5": {
+//     id: 5,
+//     time: "4pm",
+//   }
+// };
 
 export default function Application() {
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
 
-  //
+  // const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const setDay = day => setState({...state, day});
+
   useEffect(() => {
-    const newURL = "localhost:8001/api/days";
-    axios.get(newURL)
-    .then(res => console.log(res.data))
-  }, []);
-  //
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then((all) => {
+      setState({
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      })
+    })
+  });
   
-  let schedule = Object.values(appointments).map(appointment => {
+
+  let schedule = dailyAppointments.map(appointment => {
     return (
       <Appointment 
         key={appointment.id}
-        {...appointment}
+        // {...appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={appointment.interview}
       />
       // <pre>{JSON.stringify(appointment, null, 2)}</pre>
     )
@@ -75,8 +97,8 @@ export default function Application() {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={"Monday"}
+          days={state.days}
+          day={state.day}
           onChange={day => console.log(day)}
         />
       </nav>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,24 +1,9 @@
 export function getAppointmentsForDay(state, day) {
-  let outputArr = [];
-  if (state.days.length === 0) {
-    return [];
-  }
+  const selectedObjEins = state.days.find(({name}) => name === day);
+  if (!selectedObjEins) return [];
+  return selectedObjEins.appointments.map(x => state.appointments[x]);
+}
 
-  let stateCheck = 0;
-  state.days.forEach(aaa => {
-    stateCheck += (day === aaa.names ? 1 : 0 );
-  })
-  if (stateCheck === 0) {
-    return [];
-  }
-  
-  const selectedIDs = state.days.filter(selectedDay => selectedDay.name === day);
-  let selectedIDsArr = selectedIDs[0].appointments;
-  console.log(`IDsArr: ${selectedIDsArr}`);
-  selectedIDsArr.forEach(ident => {
-    let selectedAppointment = state.appointments[ident];
-    outputArr.push(selectedAppointment);
-  })
-  console.log(outputArr);
-  return outputArr; 
+export function getInterview(state, interview) {
+  console.log(interview);
 }

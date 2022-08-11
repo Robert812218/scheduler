53d489615c8e1a9d3aba0917b9721fece4857e28
Robert812218 <robert812@shaw.ca>
2022-08-10 21:49:06 -0600

    final

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -12,16 +12,22 @@ export default function useApplicationData() {
   
     const setDay = day => setState({...state, day});
 
-    // function findDay(day) {
-    //   const daysOfWeek = {
-    //     Monday: 0,
-    //     Tuesday: 1,
-    //     Wednesday: 2,
-    //     Thursday: 3,
-    //     Friday: 4
-    //   }
-    //   return daysOfWeek[day];
-    // }
+    function findDay(day) {
+      const daysOfWeek = {
+        Monday: 0,
+        Tuesday: 1,
+        Wednesday: 2,
+        Thursday: 3,
+        Friday: 4
+      }
+      return daysOfWeek[day];
+    }
+
+    useEffect(() => {
+      Promise.all([
+        
+      ])
+    })
   
     function bookInterview(id, interview) {
       const appointment = {
@@ -48,23 +54,23 @@ export default function useApplicationData() {
       };
     }
 
-    // const dayOfWeek = findDay(state.day);
+    const dayOfWeek = findDay(state.day);
 
-    // let day = {
-    //   ...state.days[dayOfWeek],
-    //   spots: state.days[dayOfWeek]
-    // }
-    // if (!state.appointments) {
-    //   day.spots = state.days[dayOfWeek].spots - 1
-    // } else {
-    //   day = {
-    //     ...state.days[dayOfWeek],
-    //     spots: state.days[dayOfWeek].spots
-    //   }
-    // }
+    let day = {
+      ...state.days[dayOfWeek],
+      spots: state.days[dayOfWeek]
+    }
+    if (!state.appointments) {
+      day.spots = state.days[dayOfWeek].spots - 1
+    } else {
+      day = {
+        ...state.days[dayOfWeek],
+        spots: state.days[dayOfWeek].spots
+      }
+    }
 
-    // let days = state.days
-    // days[dayOfWeek] = day;
+    let days = state.days
+    days[dayOfWeek] = day;
 
     function cancelInterview(id) {
       const appointment = {
@@ -77,15 +83,15 @@ export default function useApplicationData() {
         [id]: appointment
       }
 
-      // const thisWeekDay = findDay(state.day)
+      const thisWeekDay = findDay(state.day)
 
-      // const day = {
-      //   ...state.days[dayOfWeek],
-      //   spots: state.days[dayOfWeek].spot + 1
-      // }
+      const day = {
+        ...state.days[dayOfWeek],
+        spots: state.days[dayOfWeek].spot + 1
+      }
 
-      // let days = state.days
-      // days[dayOfWeek] = day;
+      let days = state.days
+      days[dayOfWeek] = day;
 
       return axios.delete("/api/appointments:id", {})
       .then(res => {
@@ -109,15 +115,8 @@ export default function useApplicationData() {
           appointments: appointments.data,
           interviewers: interviewers.data,
         })
-        getInterviewersForDay({
-          ...state,
-          // days: all[0].data,
-          days: days.data,
-          appointments: appointments.data,
-          interviewers: interviewers.data,
-        }, state.day);
       })
-    }, [state.days]);
+    }, []);
 
     return (
       state,

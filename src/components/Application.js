f6538d33933891d5ca9a6d0050740a84c4e72e78
Robert812218 <robert812@shaw.ca>
2022-08-10 21:51:17 -0600

    finish

--- src/components/Application.js
+++ src/components/Application.js
@@ -18,51 +18,43 @@ export default function Application(props) {
   console.log("## state", state);
   
 
-  // const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  // let schedule = dailyAppointments.map(appointment => {
-  //   return (
-  //     <Appointment 
-  //       key={appointment.id}
-  //       id={appointment.id}
-  //       time={appointment.time}
-  //       interviewers={appointment.interviewer}
-  //       interview={appointment.interview}
-  //     />
-  //     // <pre>{JSON.stringify(appointment, null, 2)}</pre>
-  //   )
-  // });
-
-  // // console.log(getInterviewersForDay(state, state.day));
-  // return (
-  //   <main className="layout">
-  //     <section className="sidebar">
-  //     <img
-  //       className="sidebar--centered"
-  //       src="images/logo.png"
-  //       alt="Interview Scheduler"
-  //     />
-  //     <hr className="sidebar__separator sidebar--centered" />
-  //     <nav className="sidebar__menu">
-  //       <DayList
-  //         days={state.days}
-  //         day={state.day}
-  //         onChange={day => console.log(day)}
-  //       />
-  //     </nav>
-  //     <img
-  //       className="sidebar__lhl sidebar--centered"
-  //       src="images/lhl.png"
-  //       alt="Lighthouse Labs"
-  //     />
-  //     </section>
-  //     <section>{schedule}</section>
-  //   </main>
-  // ); 
+  let schedule = dailyAppointments.map(appointment => {
+    return (
+      <Appointment 
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interviewers={appointment.interviewer}
+        interview={appointment.interview}
+      />
+    )
+  });
 
   return (
-    <div>
-      <h1>AAAAA</h1>
-    </div>
-  )
+    <main className="layout">
+      <section className="sidebar">
+      <img
+        className="sidebar--centered"
+        src="images/logo.png"
+        alt="Interview Scheduler"
+      />
+      <hr className="sidebar__separator sidebar--centered" />
+      <nav className="sidebar__menu">
+        <DayList
+          days={state.days}
+          day={state.day}
+          onChange={day => console.log(day)}
+        />
+      </nav>
+      <img
+        className="sidebar__lhl sidebar--centered"
+        src="images/lhl.png"
+        alt="Lighthouse Labs"
+      />
+      </section>
+      <section>{schedule}</section>
+    </main>
+  ); 
 }
